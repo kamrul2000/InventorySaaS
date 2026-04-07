f8c158d7031cb6a89ace0998eaf09172030bfb6a
kamrul2000 <kamrulash39@gmail.com>
2026-04-07 11:45:43 +0600

    fix: update apiUrl in environment files for production and development

--- InventorySaaS/inventory-saas-web/src/environments/environment.prod.ts
+++ InventorySaaS/inventory-saas-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'https://inventorysaas-hvgcdbg2cmf5g2h6.southeastasia-01.azurewebsites.net'
 };
--- InventorySaaS/inventory-saas-web/src/environments/environment.ts
+++ InventorySaaS/inventory-saas-web/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5179',
+  // apiUrl: 'http://localhost:5179',
+  apiUrl: 'https://inventorysaas-hvgcdbg2cmf5g2h6.southeastasia-01.azurewebsites.net'
 };
